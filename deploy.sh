3a34667a8697351c9fbb2ed2caa44fbbf0c0dddf
Sparsh Paliwal <sparshpaliwal@Sparshs-MacBook-Pro.local>
2020-12-25 00:41:35 +0530

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,7 @@ sed -i '' 1d ./components/ProjectCard.js;
 sed -i '' 's/<Image/<img/g' ./pages/index.js;
 sed -i '' 1d ./pages/index.js;
 npm run export ;
+sleep 15;
 cp -R /Users/sparshpaliwal/Desktop/repos/portfolio/public/out/. /Users/sparshpaliwal/Desktop/repos/notablog-starter/public/.
 git checkout . ; 
 
@@ -22,4 +23,3 @@ git commit -m "Deploy" ;
 git cherry-pick 13514e6de910fd3edfdb9a1614ddfb1a4c76a1a7 ;
 git push origin -f master;
 cd ..
-
